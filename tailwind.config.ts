01db8ca433f107a860dc4db46fdec2dddab0ab63
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-09-16 17:31:03 -0500

    Initial project structure

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                "purple-basic": "rgba(99, 49, 138, 1)",
+                "purple-bright": "rgba(161, 40, 255, 1)",
+                "purple-light": "rgba(196, 140, 239, 1)",
+                "purple-hover": "rgba(131, 71, 178, 1)",
+                "purple-dark": "rgba(81, 29, 122, 1)",
+                "grey-lines": "rgba(179, 179, 179, 1)",
+                "green-basic": "rgba(49, 174, 114, 1)",
+                "text-dark": "rgba(0, 0, 0, 1)",
+                "text-light": "rgba(255, 255, 255, 1)",
+                "text-purple-dark": "rgba(54, 43, 62, 1)",
+                "text-disable": "rgba(118, 104, 130, 1)",
+            },
+            
+        },
+    },
+    plugins: [],
+};
+export default config;
