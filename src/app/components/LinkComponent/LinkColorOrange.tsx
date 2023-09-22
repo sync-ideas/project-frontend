160d00d5c47678ea63a348ff30b950d5e5c9b31f
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-09-22 14:48:59 -0500

    [FEAT] link component colors

---
+++ src/app/components/LinkComponent/LinkColorOrange.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+interface LinkProps {
+    link: string;
+    text: string;
+}
+
+const LinkComponent: React.FC<LinkProps> = (props) => {
+    return (
+        <Link
+            className="text-base leading-6 text-text-disable hover:text-text-purple-dark hover:underline-offset-4 hover:underline hover:decoration-orange-basic hover:decoration-4 transition-all duration-300`}"
+            href={props.link}
+        >
+            {props.text}
+        </Link>
+    );
+};
+
+export default LinkComponent;
