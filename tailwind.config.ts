63bcd6ede0d952dc3a6e55f48207f15608c2459a
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-09-25 10:09:26 -0500

    [FIX] tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,24 +14,44 @@ const config: Config = {
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
             colors: {
-                "purple-basic": "rgba(99, 49, 138, 1)",
-                "purple-bright": "rgba(161, 40, 255, 1)",
-                "purple-light": "rgba(196, 140, 239, 1)",
-                "purple-hover": "rgba(131, 71, 178, 1)",
-                "purple-hover-light": "rgba(151, 61, 221, 1)",
-                "purple-dark": "rgba(81, 29, 122, 1)",
-                "grey-lines": "rgba(179, 179, 179, 1)",
-                "green-basic": "rgba(49, 174, 114, 1)",
-                "orange-basic": "rgba(235, 91, 39, 1)",
-                "text-dark": "rgba(0, 0, 0, 1)",
-                "text-light": "rgba(255, 255, 255, 1)",
-                "text-purple-dark": "rgba(54, 43, 62, 1)",
-                "text-disable": "rgba(118, 104, 130, 1)",
+                // Purple Shades
+                purple: {
+                    DEFAULT: "rgba(99, 49, 138, 1)", // Purple Basic
+                    neon: "rgba(161, 40, 255, 1)", // Purple Neon
+                    light: "rgba(196, 140, 239, 1)", // Light Purple
+                    hover: "rgba(131, 71, 178, 1)", // Purple Hover
+                    hoverLight: "rgba(151, 61, 221, 1)", // Purple Hover Light
+                    dark: "rgba(81, 29, 122, 1)", // Dark Purple
+                    text: "rgba(54, 43, 62, 1)", // Purple Text Dark
+                    disabled: "rgba(118, 104, 130, 1)", // Disabled Text
+                    card: "rgba(122, 82, 153, 0.9)", // Card Background
+                },
+
+                // Green Shades
+                green: {
+                    DEFAULT: "rgba(49, 174, 114, 1)", // Green Basic
+                    card: "rgba(31, 139, 88, 0.9)", // Card Background
+                },
+
+                // Orange Shades
+                orange: {
+                    DEFAULT: "rgba(235, 91, 39, 1)", // Orange Basic
+                    card: "rgba(255, 111, 58, 0.8)", // Card Background
+                },
+
+                // Grey Shades
+                gray: {
+                    DEFAULT: "rgba(179, 179, 179, 1)", // Grey Basic
+                },
+
+                //Blue Shades
+                blue: {
+                    card: "rgba(29, 131, 224, 0.8)", // Card Background
+                },
             },
             fontFamily: {
-                'sans': '"Open Sans"',
-            }
-            
+                sans: '"Open Sans"',
+            },
         },
     },
     plugins: [],
