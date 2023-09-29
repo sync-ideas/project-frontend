6bb9dfa075fcbc064e86e990e2650dff555ee7bb
BrayanGalindez <bgalindez@unal.edu.co>
2023-09-28 22:03:28 -0500

    [FIX] update button

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -3,31 +3,32 @@ import React from "react";
 interface ButtonProps {
   text: string;
   isCompleted: boolean;
+  onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, isCompleted }) => {
+const Button: React.FC<ButtonProps> = ({ text, isCompleted, onClick }) => {
   // Clases por defecto para el botón (cuando no está completo)
   let buttonClasses =
-    "h-[50px] px-6 py-3 bg-gray rounded-[5px] justify-center items-center gap-2.5 flex";
+    "w-[312px] h-[50px] px-6 py-3 bg-gray rounded-[5px] justify-center items-center gap-2.5 flex";
 
   // Si el formulario está completo, cambia las clases y el color de fondo
   if (isCompleted) {
     buttonClasses =
-      "h-[50px] px-6 py-3 bg-purple hover:bg-purple-hover active:bg-purple-dark rounded-[5px] justify-center items-center gap-2.5 flex";
+      "w-[312px] h-[50px] px-6 py-3 bg-purple hover:bg-purple-hover active:bg-purple-dark rounded-[5px] justify-center items-center gap-2.5 flex";
   }
 
   return (
-    <div className="w-[312px] h-[50px] justify-start items-start">
-      <div
+      <button
+        type="submit"
         className={`${buttonClasses} ${
           isCompleted ? "cursor-pointer" : "cursor-not-allowed"
         }`}
+        onClick={onClick}
       >
         <div className="text-white text-base font-sans leading-snug">
           {text}
         </div>
-      </div>
-    </div>
+      </button>
   );
 };
 
