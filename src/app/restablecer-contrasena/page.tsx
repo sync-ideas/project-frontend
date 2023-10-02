09aa7fe8b932f7e8a8d275e8173b795f952bbb61
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-10-02 16:22:27 -0500

    [FEAT] added button component

--- src/app/restablecer-contrasena/page.tsx
+++ src/app/restablecer-contrasena/page.tsx
@@ -1,14 +1,16 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import logo from "../../../public/assets/images/icon.svg";
 import LinkComponent from "../components/LinkComponent/LinkComponentCustom";
 import CustomInput from "../components/input";
-import Link from "next/link";
+import Button from "../components/button";
 
 interface GetPasswordProps {}
 const GetPassword: React.FC<GetPasswordProps> = (props) => {
+
     return (
-        <div className="h-screen sm:py-10 sm:flex sm:justify-center">
+        <div className="h-screen sm:py-10 sm:flex sm:justify-center font-sans">
             <div className="flex items-center flex-col pt-[60px] px-6 sm:min-w-[360px] sm:m-auto sm:border-0 sm:shadow-md sm:max-h-full sm:min-h-[640px] sm:rounded-[20px]">
                 <Image src={logo} width={106} height={106} alt="Logo" />
                 <form action="" className="w-full">
@@ -21,12 +23,10 @@ const GetPassword: React.FC<GetPasswordProps> = (props) => {
                         />
                     </div>
                     <div className="w-full">
-                        <button
-                            className="bg-purple hover:bg-purple-hover focus:bg-purple-dark disabled:bg-grey-lines py-3 px-6 text-white w-full border-0 rounded-md font-bold"
-                            type="submit"
-                        >
-                            <Link href="">Recuperar contraseña</Link>
-                        </button>
+                        <Button
+                            text="Recuperar contraseña"
+                            isCompleted={true}
+                        />
                     </div>
                 </form>
                 <div className="w-full text-right mt-4">
