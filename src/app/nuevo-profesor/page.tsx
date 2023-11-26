389e01429e798cc276745019430c7b97d6c85e69
Rafaric <rafaelstrongoli@gmail.com>
2023-11-26 19:43:20 -0300

    deleted font config. fix spelling error

--- src/app/nuevo-profesor/page.tsx
+++ src/app/nuevo-profesor/page.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 import Footer from "../components/Footer";
-import { Open_Sans } from "next/font/google";
 import Breadcrumb from "../components/Breadcrumb";
 import Link from "next/link";
 import ProfForm from "../components/ProfForm";
 import NavBar from "../components/navbar";
 import LinkComponent from "../components/LinkComponent/LinkComponentCustom";
 
-const open_sans = Open_Sans({ subsets: ["latin"] });
-
 const NewProfessor = () => {
   return (
-    <div
-      className={`mx-auto h-screen ${open_sans.className} text-base flex flex-col items-center`}
-    >
+    <div className={`mx-auto h-screen text-base flex flex-col items-center`}>
       <div className="w-full flex flex-col items-center">
         <NavBar />
         <div className="flex justify-between w-[85%] md:w-[95%] xl:w-[82%]">
           <Breadcrumb
             links={[
               { hiper: "/", text: "Profesores" },
-              { hiper: "", text: "Nuevo Perfil" },
+              { hiper: "", text: "Nuevo perfil" },
             ]}
           />
           <LinkComponent
