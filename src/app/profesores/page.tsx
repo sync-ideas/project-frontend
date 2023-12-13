65ba87abfdd1e887ba33f04269abf434dc32d329
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-12-13 11:08:14 -0500

    [FIX] width and padding size

--- src/app/profesores/page.tsx
+++ src/app/profesores/page.tsx
@@ -12,13 +12,9 @@ const Professor: React.FC<ProfessorProps> = () => {
     return (
         <div className="max-h-screen">
             <NavBar />
-            <div className="px-6 sm:px-8 md:px-[120px] py-[10px]">
+            <div className="px-6 xl:px-[120px] py-[10px]">
                 <div className="w-full flex justify-between mb-[10px]">
-                    <Breadcrumb
-                        links={[
-                            { hiper: "/", text: "Profesores" }
-                        ]}
-                    />
+                    <Breadcrumb links={[{ hiper: "/", text: "Profesores" }]} />
                 </div>
                 <Link href="/nuevo-profesor" className="">
                     <div className="bg-purple bg-opacity-20 rounded-[5px] w-full flex items-center justify-center flex-col opacity-70 hover:opacity-100 px-20 h-[422px] sm:max-h-screen md:min-h-screen xl:min-h-full">
@@ -30,13 +26,15 @@ const Professor: React.FC<ProfessorProps> = () => {
                             className="w-16 h-16 mb-6"
                             style={{ color: "rgba(54, 43, 62, 1)" }}
                         />
-                        <p className="text-center text-purple-text">
-                            Agrega al primer profesor del equipo docente
+                        <p className="text-center text-purple-text font-semibold text-base">
+                            Agrega al primer <br />
+                            profesor del <br />
+                            equipo docente
                         </p>
                     </div>
                 </Link>
                 <div className="mt-[10px] sm:mt-6 w-full flex justify-end">
-                    <Link href="/nuevo-profesor" className="w-full xl:w-1/5">
+                    <Link href="/nuevo-profesor" className="w-full xl:w-1/5 min-w-[320px]">
                         <Button text="Nuevo perfil" isCompleted={true} />
                     </Link>
                 </div>
