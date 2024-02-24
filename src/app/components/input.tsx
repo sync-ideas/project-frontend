66479319867328de26233b180fbf8a68a8d74f58
BrayanGalindez <bgalindez@unal.edu.co>
2024-02-24 14:51:24 -0500

    [FEAT] Implementación de vista de inicio de sesión con validaciones y conexión al backend.
    Se implementó por completo la vista de inicio de sesión, incorporando validaciones de campos y conectándola al backend para autenticación de usuarios. La siguiente fase implica la utilización del token de inicio de sesión para acceder a distintas secciones del sitio y para que el sistema identifique cuando un usuario inicia sesión.

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -3,40 +3,58 @@ import React, { useState } from "react";
 import eyeOpen from "../../../public/assets/images/eyeOpen.svg";
 import eyeClose from "../../../public/assets/images/eyeClose.svg";
 import Image from "next/image";
+import { UseFormRegister, FieldValues } from "react-hook-form";
 
 interface CustomInputProps {
+  id:string;
   placeholder?: string;
   pass?: boolean;
   type: string;
   label?: string;
-  method?: (event: React.ChangeEvent<HTMLInputElement>) => void
-  value: string;
+  method?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  register: UseFormRegister<FieldValues>;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  error: boolean;
+  onClick: (event: React.MouseEvent<HTMLInputElement>) => void;
 }
 
 const CustomInput: React.FC<CustomInputProps> = ({
+  id,
   placeholder,
   pass = false,
   type,
   label,
   method,
-  value,
+  register,
+  onChange,
+  error,
+  onClick 
 }) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleTogglePassword = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
-
+  const isError = error;
   return !pass ? (
     <div className="flex flex-col">
       <label htmlFor="input">{label}</label>
       <input
-        id="input"
-        className="px-2 py-2 h-14 w-full placeholder:text-purple-hover border-2 border-purple rounded-lg font-normal focus:outline-purple"
+        id={id}
+        className={`px-2 py-2 h-[50px] w-[312px] placeholder:text-purple-hover border-2 rounded-lg font-normal  ${isError ? 'border-[#DE1111] focus:outline-[#DE1111]' : 'border-purple focus:outline-purple'}`}
         type={type}
         placeholder={placeholder}
-        value={value}
-        onChange={method}
+        {...register(id, {
+          onChange: (e) => {
+            if (method) {
+              method(e);
+            }
+            if (onChange) {
+              onChange(e);
+            }
+          },
+        })}
+        onClick={onClick}
       />
     </div>
   ) : (
@@ -44,18 +62,26 @@ const CustomInput: React.FC<CustomInputProps> = ({
       <div className="flex flex-col">
         <label htmlFor="input">{label}</label>
         <input
-          id="input"
-          className="px-2 py-2 h-14 w-full placeholder:text-purple-hover border-2 border-purple rounded-lg font-normal focus:outline-purple"
-          type={showPassword ? "text" : "password"}
+          id={id}
+          className={`px-2 py-2 h-[50px] w-[312px] placeholder:text-purple-hover border-2 rounded-lg font-normal focus:outline-purple ${isError ? 'border-[#DE1111] focus:outline-[#DE1111]' : 'border-purple focus:outline-purple'}`}
           placeholder={placeholder}
-          value={value}
-          onChange={method}
+          {...register(id, {
+            onChange: (e) => {
+              if (method) {
+                method(e);
+              }
+              if (onChange) {
+                onChange(e);
+              }
+            },
+          })}
+          onClick={onClick}
         />
         <div className="absolute top-1/2 right-3 cursor-pointer z-10" onClick={handleTogglePassword}>
           {showPassword ? (
-            <Image src={eyeOpen} alt="algo" className="w-6 h-6 text-purple"/>
+            <Image src={eyeOpen} alt="algo" className="w-6 h-6" priority/>
           ) : (
-            <Image src={eyeClose} alt="algo" className="w-6 h-6 text-purple"/>
+            <Image src={eyeClose} alt="algo" className="w-6 h-6" priority/>
           )}
         </div>
       </div>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,96 +1,183 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useForm, Controller } from "react-hook-form";
-
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userSchema } from "@/validations/userSchema";
 import Image from "next/image";
 import logo from "../../../public/assets/images/icon.svg";
 import Button from "../components/button";
 import CustomInput from "../components/input";
 import LinkComponent from "../components/LinkComponent/LinkComponentCustom";
 import { Checkbox } from "../components/Checkbox";
+import { areInputsNotEmpty } from "../functions/input/formUtils";
 
 interface LoginProps {}
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 const Login: React.FC<LoginProps> = (props) => {
+
+  
+  //Funcion para dirigir a otra pagina en caso de respuesta positiva de login
   const router = useRouter();
+
+  //Formulario con react-hook-form y validaciones iniciales con zod
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm<Inputs>({
+    resolver: zodResolver(userSchema),
+  });
+
+  //Estado de checkbox para guardar los datos registrados en los input
   const [isChecked, setIsChecked] = useState(false);
-  const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
+  //Estado de boton para activarlo
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  //Estados de inputs para cambiar estilos si hay errores
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
-  const handleUser = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setUser(value);
-    setIsButtonDisabled(value === "" || password === "");
+
+  //Validacion de inputs con valor para activar el boton
+  const handleInputChange = useCallback(() => {
+    const inputsNotEmpty = areInputsNotEmpty(watch, "email", "password");
+    setIsButtonDisabled(!inputsNotEmpty);
+  }, [watch, setIsButtonDisabled]);
+
+
+  // Lógica para manejar el clic en el input y actualizar los estados de error
+  const handleInputClick = (inputId) => {
+    if (inputId === "email" || inputId === "password") {
+      setEmailError(false);
+      setPasswordError(false);
+    }
   };
 
-  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setPassword(value);
-    setIsButtonDisabled(value === "" || user === "");
+ // Verificar si hay datos guardados en el localStorage y establecerlos
+  useEffect(() => {
+    const savedUserData = localStorage.getItem("savedUserData");
+    if (savedUserData) {
+      const parsedData = JSON.parse(savedUserData);
+      setValue("email", parsedData.email);
+      setValue("password", parsedData.password);
+      handleInputChange(); // Actualizar el estado del botón después de establecer los valores
+    }
+  }, [setValue, handleInputChange]);
+  
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        "https://attendance-control-sync-ideas.vercel.app/api/users/login",
+        {
+          email: data.email,
+          password: data.password,
+        }
+      );
+      console.log(response.data);
+      // Si la peticion es exitosa redirige a home
+      router.push("/home");
+      // Guarda los datos si la casilla "Recordarme" está marcada
+      if (isChecked) {
+        localStorage.setItem("savedUserData", JSON.stringify(data));
+      }
+      console.log(localStorage.savedUserData)
+    } catch (error) {
+      console.error(error.response.data.message);
+      // Si hay un error, actualiza los estados de error correspondientes
+      if (error.response.data.message.includes("User not found")) {
+        setEmailError(true);
+      }
+      if (error.response.data.message.includes("Incorrect password")) {
+        setPasswordError(true);
+      }
+    }
   };
 
-  const handleClick = (e: any) => {
-    // Logica al dar click en el button
-    router.push("/home");
-    console.log("Clicked!", e);
-  };
-  const onClickHandler: () => void = () => {
-    handleClick(undefined);
-  };
-
-  const { control, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    // Realizar acciones con los datos del formulario, conexion a backend
-    console.log(data);
-  };
   return (
     <div className="h-screen sm:py-10 sm:flex sm:justify-center">
       <div className="flex items-center flex-col pt-[60px] px-6 sm:min-w-[360px] sm:m-auto sm:border-0 sm:shadow-md sm:max-h-full sm:min-h-[640px] sm:rounded-[20px]">
-        <Image src={logo} width={106} height={106} alt="logo" />
+        <Image src={logo} width={106} height={106} alt="logo" priority />
         <form onSubmit={handleSubmit(onSubmit)} className="w-full my-2">
+          {/* <form onSubmit={handleSubmit(data => {console.log(data)})} className="w-full my-2"> */}
           <div className="pt-5 w-full">
             <CustomInput
+              id="email"
               placeholder="Ingresa tu nombre de usuario"
               pass={false}
               type="text"
               label="Usuario"
-              value={user}
-              method={handleUser}
+              register={register}
+              onChange={handleInputChange}
+              error={emailError}
+              onClick={() => handleInputClick("email")}
             />
+
+            {/* Validaciones iniciales de formulario */}
+            {errors.email?.message && (
+              <p className="w-[320px] mt-[5px] text-[#DE1111]">
+                {errors.email.message}
+              </p>
+            )}
+
+            {/* Mostrar mensaje de error del backend */}
+            {emailError && (
+              <p className="w-[320px] mt-[5px] text-[#DE1111]">
+                Usuario no registrado.
+              </p>
+            )}
           </div>
           <div className=" my-2 w-full">
             <CustomInput
+              id="password"
               placeholder="Ingresa tu contraseña"
               pass={true}
               type="password"
               label="Contraseña"
-              value={password}
-              method={handlePassword}
+              register={register}
+              onChange={handleInputChange}
+              error={passwordError}
+              onClick={() => handleInputClick("password")}
             />
+
+            {/* Validaciones iniciales de formulario */}
+            {errors.password?.message && (
+              <p className="w-[320px] mt-[5px] text-[#DE1111]">
+                {errors.password.message}
+              </p>
+            )}
+
+            {/* Mostrar mensaje de error del backend */}
+            {passwordError && (
+              <p className="w-[320px] mt-[5px] text-[#DE1111]">
+                Contraseña incorrecta.
+              </p>
+            )}
           </div>
           <div className="w-full text-left mt-2 flex items-center mb-4">
             <Checkbox isChecked={isChecked} setIsChecked={setIsChecked} />
             <a className="ml-2">Recordarme</a>
           </div>
           <div className="w-full">
-            <Button
-              text="Iniciar sesión"
-              isCompleted={!isButtonDisabled}
-              onClick={!isButtonDisabled ? onClickHandler : undefined}
-            />
+            <Button text="Iniciar sesión" isCompleted={!isButtonDisabled} />
           </div>
           <div className="w-full text-center mt-4">
             <LinkComponent
               link="/restablecer-contrasena"
               text="He olvidado mi contraseña"
-              decorationColor="orange"
-              textColor="purple-disabled"
-              textColorHover="purple"
+              decorationColor="hover:decoration-orange"
+              textColor="text-purple-disabled"
+              textColorHover="hover:text-purple-text"
             />
           </div>
         </form>
+        {/* <div className="w-[320px]">{JSON.stringify(watch(), null, 2)}</div> */}
       </div>
     </div>
   );
