6bde0a5366475697e82c2a0f038fe3cee21c36bf
BrayanGalindez <bgalindez@unal.edu.co>
2024-05-08 19:11:17 -0500

    [FIX] Formulario actualizado

--- src/app/restablecer-contrasena/page.tsx
+++ src/app/restablecer-contrasena/page.tsx
@@ -1,76 +1,119 @@
 "use client";
-import React, { useState } from "react";
+import { useState, useCallback } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userSchema } from "../../validations/userSchemaRestore";
 import Image from "next/image";
 import logo from "../../../public/assets/images/icon.svg";
 import CustomInput from "../components/input";
 import Button from "../components/button";
 import LinkComponent from "../components/LinkComponent/LinkComponentCustom";
-import axios from "axios";
+import { areInputsNotEmpty } from "../functions/input/formUtils";
 
 interface GetPasswordProps {}
 
-const GetPassword: React.FC<GetPasswordProps> = (props) => {
-  const [email, setEmail] = useState("");
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  const [error, setError] = useState("");
+type Inputs = {
+  email: string;
+};
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setEmail(value);
-    setIsButtonDisabled(value === "");
-    setError("");
+const GetPassword: React.FC<GetPasswordProps> = (props) => {
+  //Funcion para dirigir a otra pagina en caso de respuesta positiva de login
+  const router = useRouter();
+
+  //Formulario con react-hook-form y validaciones iniciales con zod
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<Inputs>({
+    resolver: zodResolver(userSchema),
+  });
+
+  //Estado de boton para activarlo
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  //Estados de inputs para cambiar estilos si hay errores
+  const [emailError, setEmailError] = useState(false);
+
+  //Validacion de inputs con valor para activar el boton
+  const handleInputChange = useCallback(() => {
+    const inputsNotEmpty = areInputsNotEmpty(watch, "email");
+    setIsButtonDisabled(!inputsNotEmpty);
+  }, [watch, setIsButtonDisabled]);
+
+  // Lógica para manejar el clic en el input y actualizar los estados de error
+  const handleInputClick = (inputId: string) => {
+    if (inputId === "email") {
+      setEmailError(false);
+    }
   };
 
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    //TODO: Agregar código de envio de información a la bd
-    e.preventDefault();
+  const onSubmit = async (data: Inputs) => {
     try {
-      const respuesta = await axios.post(
-        "https://attendance-control-sync-ideas.vercel.app/api/users/forgotpassword",
+      const response = await axios.post(
+        "https://attendance-control.vercel.app/api/users/forgotpassword",
         {
-          email: email,
+          email: data.email,
         }
       );
-      if (respuesta.status === 200) {
-        alert(
-          "Se ha enviado un correo electrónico para restablecer la contraseña"
-        );
+      // console.log(response.data);
+      // Si la peticion es exitosa redirige a mensaje-enviado
+      router.push("/mensaje-enviado");
+    } catch (error: any) {
+      // console.error(error.response.data.message);
+      // Si hay un error, actualiza los estados de error correspondientes
+      if (error.response.data.message.includes("User not found")) {
+        setEmailError(true);
       }
-    } catch (error) {
-      setError("Este correo electrónico no está registrado.");
     }
   };
 
   return (
     <div className="h-screen sm:py-10 sm:flex sm:justify-center">
       <div className="flex items-center flex-col pt-[60px] px-6 sm:min-w-[360px] sm:m-auto sm:border-0 sm:shadow-md sm:max-h-full sm:min-h-[640px] sm:rounded-[20px]">
-        <Image src={logo} width={106} height={106} alt="logo" />
-        <form action="" className="w-full">
+        <Image src={logo} width={106} height={106} alt="logo" priority />
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
           <div className="pt-5 my-2 w-full">
             <CustomInput
+              id="email"
               placeholder="Ingresa tu correo electrónico"
               pass={false}
-              type="email"
+              type="text"
               label="Correo electrónico"
-              value={email}
-              method={handleEmailChange}
+              register={register}
+              onChange={handleInputChange}
+              error={emailError}
+              onClick={() => handleInputClick("email")}
             />
+            {/* Validaciones iniciales de formulario */}
+            {errors.email?.message && (
+              <p className="w-[320px] mt-[5px] text-[#DE1111]">
+                {errors.email.message}
+              </p>
+            )}
+
+            {/* Mostrar mensaje de error del backend */}
+            {emailError && (
+              <p className="w-[320px] mt-[5px] text-[#DE1111]">
+                Usuario no registrado.
+              </p>
+            )}
           </div>
-          <p className="text-[#DE1111] font-normal text-sm my-2">{error}</p>
           <div className="w-full">
             <Button
               text="Recuperar contraseña"
               isCompleted={!isButtonDisabled}
-              onClick={handleSubmit}
             />
           </div>
           <div className="w-full text-right mt-4">
             <LinkComponent
               link="/login"
               text="< Volver a Iniciar Sesión"
-              decorationColor="green"
-              textColor="purple-disabled"
-              textColorHover="purple"
+              decorationColor="hover:decoration-green"
+              textColor="text-purple-disabled"
+              textColorHover="hover:text-purple"
             />
           </div>
         </form>
